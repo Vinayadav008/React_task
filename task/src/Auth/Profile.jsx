fb413ae85cc9bb5b2ca0b59621959cc8b0e99104
bulbul1434 <bulbultomar14@gmail.com>
2024-04-06 12:42:19 +0530

    hey

--- task/src/Auth/Profile.jsx
+++ task/src/Auth/Profile.jsx
@@ -3,15 +3,15 @@ function Profile() {
     return (
         <>
             <div className='container'>
-                <div>Logo</div>
+                <div>logo</div>
                 <div className='flex justify-center p-2'>
                     <div className=''>
-                        <h1 className='font-bold text-xl sm:text-3xl'>Welocome! Lets create your profile</h1>
-                        <p className='text-secondary font-semibold text:lg sm:text-lg'>Let other get to know you better! You can do these later</p>
+                        <h1 className='font-bold text-xl sm:text-3xl'>Welcome! Let's create your profile</h1>
+                        <p className='text-secondary font-semibold text:lg sm:text-lg'>Let others get to know you better! You can do these later</p>
 
                         <div className='pt-8'>
                             <form action="">
-                                <h2 className='font-bold text-lg sm:text-xl'>Add an avtar</h2>
+                                <h2 className='font-bold text-lg sm:text-xl'>Add an avatar</h2>
                                 <div className="flex gap-8 pt-4 items-center">
                                     <div className='border-2 border-secondary text-center w-32 h-32 flex items-center justify-center border-dotted rounded-full'>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M29 26H3a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h6.46l1.71-2.55A1 1 0 0 1 12 4h8a1 1 0 0 1 .83.45L22.54 7H29a1 1 0 0 1 1 1v17a1 1 0 0 1-1 1M4 24h24V9h-6a1 1 0 0 1-.83-.45L19.46 6h-6.92l-1.71 2.55A1 1 0 0 1 10 9H4Z" /><path fill="currentColor" d="M16 22a6 6 0 1 1 6-6a6 6 0 0 1-6 6m0-10a4 4 0 1 0 4 4a4 4 0 0 0-4-4" /></svg>
--- task/src/Home/Header.jsx
+++ task/src/Home/Header.jsx
@@ -3,8 +3,20 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <div className="bg-secondary text-white">Header</div>
-      <p>Hello i am Bulbul </p>
+      <div className="bg-secondary text-white">
+        <div className='flex m-1 '>
+          <div>
+            <img src="./images/logo1.png" alt="logo" />
+          </div>
+          <div className=''>
+            <a href="#">Inspiration</a>
+            <a href="#">Find Work</a>
+            <a href="#">Learn Design</a>
+            <a href="#">Go Pro</a>
+            <a href="#">Hire Designers</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
